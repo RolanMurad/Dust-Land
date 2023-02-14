fb1e16f5a14965435ff38c02de649f50555d5bc3
Rolan Murad <rolanmuraddeveloper@gmail.com>
2023-02-14 19:17:23 +0100

    Section Weapons

--- src/js/jquery-scripts.js
+++ src/js/jquery-scripts.js
@@ -11,4 +11,15 @@ $(function () {
     slidesToScroll: 1,
     arrows: false,
   });
+
+  //!Slick-slider
+  $('.weapons__slider').slick({
+    //!Main settings
+    dots: true,
+    autoplay: true,
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false,
+  });
 })
